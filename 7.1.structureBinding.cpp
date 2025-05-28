bff3f610a553695b5b0c87960af4c0f465540f60
haseeb khan <haseebkhanbettani@gmail.com>
2025-05-28 16:10:11 +0500

    Structure Binding. dealing with Multiple return type in function by using Tuple and along with auto key word.
    Signed-off-by: haseeb khan <haseebkhanbettani@gmail.com>

--- 7.1.structureBinding.cpp
+++ 7.1.structureBinding.cpp
@@ -9,9 +9,31 @@ tuple < string, int > CreatePerson()
     return {"Haseeb", 20};
 }
 
+struct Person
+{
+    string name;
+    int age;
+};
+
+
 // dealing with Multiple return type.
 int main()
 {
+
+    Person p[4];
+    p[0].name = "Hasnain";
+    p[0].age = 20;
+
+    p[1].name = "Ayesha";
+    p[1].age = 22;
+
+    p[2].name = "Ali";
+    p[2].age = 25;
+
+    p[3].name = "Sara";
+    p[3].age = 18;
+
+
     auto person = CreatePerson();
     string name = get<0>(person);
     int age = get<1>(person);
@@ -23,6 +45,27 @@ int main()
     auto [name1, age1] = CreatePerson();
     cout << "Name: " << name1 << " Age: " << age1 << endl;
     // =================================================================================/
+
+    cout << "\n\n\n\n\n";
+
+    for(auto&[name, age] : p)
+    {
+        cout << "Name: " << name << " Age: " << age << endl; 
+    }
+
+    cout << "\n\n\n\n\n";
+
+    for (auto& psvalue : p)
+    {
+        string name = psvalue.name;
+        int age = psvalue.age;
+
+        cout << "Name: " << name << " Age: " << age << endl; 
+
+    }
+    
+
+    
     
     return 0;
 }

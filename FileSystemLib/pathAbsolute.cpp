596178e7da231cae72cf473e34bf28a87a0f9550
haseeb khan <haseebkhanbettani@gmail.com>
2025-08-26 22:05:24 +0500

    File System -> path and Absolute.
    Signed-off-by: haseeb khan <haseebkhanbettani@gmail.com>

---
+++ FileSystemLib/pathAbsolute.cpp
@@ -0,0 +1,58 @@
+
+// Example: Throwing version (no ec)
+// fs::path p = "foo.c";
+// fs::path abs = fs::absolute(p);  // Throws on error
+
+// Example: Non-throwing version (with ec)
+// fs::path p = "foo.c";
+// std::error_code ec;
+// fs::path abs = fs::absolute(p, ec); // Won’t throw, reports error via ec
+
+// if (ec) {
+//     std::cout << "Error: " << ec.message() << '\n';
+// } else {
+//     std::cout << "Absolute path is " << abs << '\n';
+// }
+
+
+// So in plain English:
+
+// p is the path you want to make absolute.
+
+// ec is optional — if you use it, you’re asking the function not to throw exceptions, 
+// but instead to set ec with any error info.
+#include <iostream>
+#include <filesystem>
+using namespace std;
+
+namespace fs = std::filesystem;
+
+int main()
+{   
+    try
+    {
+        fs::path checkfile = "file1";
+
+        cout << "Current Path is: " << fs::current_path() << endl;
+        cout << "Absolute Path for: " << checkfile << " is: " 
+             << fs::absolute(checkfile) << endl;
+
+    }
+    catch(const fs::filesystem_error& e)
+    {
+        cerr << "\nFileSystem error: \n" <<  e.what() << '\n';
+        cerr << "Path1: " << e.path1() << endl;
+        cerr << "Path2: " << e.path2() << endl;
+
+        if (!e.path2().empty())
+        {
+            cerr << "Path2: " << e.path2() << endl;
+        }
+    }
+    catch(const exception& e)
+    {
+        cerr << "General exception: " << e.what() << endl;
+    }
+
+    return 0;
+}

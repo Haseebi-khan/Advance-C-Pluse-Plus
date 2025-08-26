aed21ba0a94c9e9042c219236d8ec1867578dbe6
haseeb khan <haseebkhanbettani@gmail.com>
2025-08-26 22:23:14 +0500

    File System -> Error Code.
    Signed-off-by: haseeb khan <haseebkhanbettani@gmail.com>

--- FileSystemLib/pathAbsolute.cpp
+++ FileSystemLib/pathAbsolute.cpp
@@ -9,21 +9,23 @@
 // fs::path abs = fs::absolute(p, ec); // Won’t throw, reports error via ec
 
 // if (ec) {
-//     std::cout << "Error: " << ec.message() << '\n';
-// } else {
-//     std::cout << "Absolute path is " << abs << '\n';
-// }
-
-
-// So in plain English:
-
-// p is the path you want to make absolute.
-
+    //     std::cout << "Error: " << ec.message() << '\n';
+    // } else {
+        //     std::cout << "Absolute path is " << abs << '\n';
+        // }
+        
+        
+        // So in plain English:
+        
+        // p is the path you want to make absolute.
+        
 // ec is optional — if you use it, you’re asking the function not to throw exceptions, 
 // but instead to set ec with any error info.
 #include <iostream>
 #include <filesystem>
 using namespace std;
+
+#include <system_error> // for std::error_code
 
 namespace fs = std::filesystem;
 
@@ -54,5 +56,21 @@ int main()
         cerr << "General exception: " << e.what() << endl;
     }
 
+    cout << "===============================================================\n";
+    cout << "=                    ITS SAFE                                 =\n";
+    cout << "===============================================================\n\n";
+
+
+    fs::path p = "";
+    std::error_code ec;
+
+    auto absPath = fs::absolute(p, ec); // pass error_code
+    if (ec) {
+        std::cerr << "Error: " << ec.message() << '\n';
+    } else {
+        std::cout << "Current path is " << fs::current_path() << '\n';
+        std::cout << "Absolute path for " << p << " is " << absPath << '\n';
+    }
+ 
     return 0;
 }
